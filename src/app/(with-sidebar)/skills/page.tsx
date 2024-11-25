9a456c6dd1211609dc7bde71feaf2fd2e3f99e27
Polo1121 <michal.jasinski11121@gmail.com>
2024-11-25 21:11:31 +0100

    Fix exports

--- src/app/(with-sidebar)/skills/page.tsx
+++ src/app/(with-sidebar)/skills/page.tsx
@@ -4,7 +4,7 @@ import { Separator } from "@/components/ui/separator";
 import { ADVANCED_skills, BASIC_SKILLS } from "@/helpers/constants";
 import { FaTools } from "react-icons/fa";
 
-export const SkillsPage = () => (
+const SkillsPage = () => (
   <Section title="My Skills" Icon={FaTools}>
     <div className="flex flex-col lg:flex-row justify-center mx-auto">
       <SkillsList skills={ADVANCED_skills} title="Advanced" />
